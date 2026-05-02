b11d1ab41ea0475bbf1e02a40edb123709a41435
Sudenur Kenar <kenarsudenur2006@gmail.com>
2026-05-03 01:12:50 +0300

    Delete src directory

--- src/scenes/Part2.js
+++
@@ -1,8 +0,0 @@
-// Visual.js içinde basit bir mantık
-visualizeBubbleSort(array); {
-    array.forEach((val, i) => {
-        // Her veri elemanı için bir mor çubuk çiz
-        let bar = this.add.rectangle(100 + (i * 30), 400, 20, val * 10, 0x9b59b6);
-        bar.setOrigin(0, 1); // Çubukların aşağıdan yukarı uzanması için
-    });
-}
